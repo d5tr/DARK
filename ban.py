fb4e581a0ffce87085ae018f92637c2c62cfc91a
D5TR <72051237+d5tr@users.noreply.github.com>
2021-07-11 00:58:56 -0400

    Add files via upload

---
+++ ban.py
@@ -0,0 +1,59 @@
+from colorama import Fore
+
+def BAN_DARK():
+
+    print(f'''
+    
+██████╗  █████╗ ██████╗ ██╗  ██╗
+██╔══██╗██╔══██╗██╔══██╗██║ ██╔╝
+██║  ██║███████║██████╔╝█████╔╝ 
+██║  ██║██╔══██║██╔══██╗██╔═██╗ 
+██████╔╝██║  ██║██║  ██║██║  ██╗
+╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝
+                                                         
+         INSTA 01 ---> @zer0one_01
+         INSTA ---> @d_5tr
+
+    {Fore.MAGENTA}[{Fore.CYAN}1{Fore.MAGENTA}]{Fore.CYAN} Searching for google dork
+    {Fore.MAGENTA}[{Fore.CYAN}2{Fore.MAGENTA}]{Fore.CYAN} Searching for tracks
+    {Fore.MAGENTA}[{Fore.CYAN}3{Fore.MAGENTA}]{Fore.CYAN} Searching for subdomin 
+    {Fore.MAGENTA}[{Fore.CYAN}99{Fore.MAGENTA}]{Fore.CYAN} Exit
+    {Fore.WHITE}
+    ''')
+
+def BAN_TRACKS():
+
+    print(f'''
+████████╗██████╗  █████╗  ██████╗██╗  ██╗███████╗
+╚══██╔══╝██╔══██╗██╔══██╗██╔════╝██║ ██╔╝██╔════╝
+   ██║   ██████╔╝███████║██║     █████╔╝ ███████╗    INSTA--> @d_5tr
+   ██║   ██╔══██╗██╔══██║██║     ██╔═██╗ ╚════██║    Team---> @zer0one_01
+   ██║   ██║  ██║██║  ██║╚██████╗██║  ██╗███████║
+   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚══════╝
+   
+    {Fore.MAGENTA}[{Fore.CYAN}1{Fore.MAGENTA}]{Fore.CYAN} PHP tracks
+    {Fore.MAGENTA}[{Fore.CYAN}2{Fore.MAGENTA}]{Fore.CYAN} HTML tracks
+    {Fore.MAGENTA}[{Fore.CYAN}3{Fore.MAGENTA}]{Fore.CYAN} WORD PREES tracks
+    {Fore.MAGENTA}[{Fore.CYAN}4{Fore.MAGENTA}]{Fore.CYAN} TRACKS from you
+    {Fore.MAGENTA}[{Fore.CYAN}99{Fore.MAGENTA}]{Fore.CYAN} Back
+    {Fore.WHITE}
+    ''')
+
+def BAN_SUBDOMIN():
+
+    print(f'''
+    
+███████╗██╗   ██╗██████╗ ██████╗  ██████╗ ███╗   ███╗██╗███╗   ██╗
+██╔════╝██║   ██║██╔══██╗██╔══██╗██╔═══██╗████╗ ████║██║████╗  ██║
+███████╗██║   ██║██████╔╝██║  ██║██║   ██║██╔████╔██║██║██╔██╗ ██║
+╚════██║██║   ██║██╔══██╗██║  ██║██║   ██║██║╚██╔╝██║██║██║╚██╗██║
+███████║╚██████╔╝██████╔╝██████╔╝╚██████╔╝██║ ╚═╝ ██║██║██║ ╚████║
+╚══════╝ ╚═════╝ ╚═════╝ ╚═════╝  ╚═════╝ ╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝
+              INSTA --> @d_5tr
+              Team --> @zer0one_01
+              
+          {Fore.MAGENTA}[{Fore.CYAN}1{Fore.MAGENTA}]{Fore.CYAN} Searching for subdomin
+          {Fore.MAGENTA}[{Fore.CYAN}2{Fore.MAGENTA}]{Fore.CYAN} Searching for subdomin but you have list file      
+          {Fore.MAGENTA}[{Fore.CYAN}99{Fore.MAGENTA}]{Fore.CYAN} Back                                                           
+{Fore.WHITE}
+    ''')
